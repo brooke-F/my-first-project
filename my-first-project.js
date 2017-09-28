017178cb39ab14a9d52080e852cd14297b05394a
brooke f <31109746+brooke-F@users.noreply.github.com>
2017-09-28 00:07:56 -0500

    brooke f project
    sorry if im submitting this wrong i dont know what im doing

--- my-first-project.js
+++ my-first-project.js
@@ -1,2 +1,6 @@
 var correct = true
-if (correct{console.log("It's correct!")}else{console.log("It's wrong!")})
+  if (correct)  {
+    console.log("It's correct!");
+    } else  {
+      console.log("It's wrong!");
+    } //why do i need all these brackets
